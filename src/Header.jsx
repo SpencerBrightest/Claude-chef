9c3e3eedafc12ed5942aa0dc98386a26e4251952
Spencer bright <spenzerbrightest@gmail.com>
2026-01-23 18:47:35 +0100

    master

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <img src="/src/assets/chef image bg remover.png" alt="" />
+      <h1>Spencer chef</h1>
+    </header> 
+  )
+}
+
+export default Header
