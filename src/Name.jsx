83a2ec1c9cb7b1e7d8fa644757aa215363eda3d8
Spencer bright <spenzerbrightest@gmail.com>
2026-02-09 15:50:49 +0100

    fixed something

--- src/Name.jsx
+++ src/Name.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 
 const Name = () => {
 
-  const ingridients = ['onions', 'tomatoes', 'cheese']
+  const [ingridients,setIngridients]=React.useState([])
 
 const ingridientsList=ingridients.map((ingridients)=>(
   <li key={ingridients}>{ingridients}</li>
 ))
 
-function submit (){
-  console.log('submitted')
+function submit (event){
+  event.preventDefault()
+  alert('submitted')
+
+  const formdata=new FormData(event.target)
+  const ingridient=formdata.get('ingridients')
+   setIngridients(ingrid=>[...ingrid,ingridient])
 }
   
   return (
@@ -19,7 +24,8 @@ function submit (){
        <form action="" className='add-ingridient-form' onSubmit={submit}>
         <input type="text"  
         arai-label="Add ingridient"
-         placeholder='e.g onions' />
+         placeholder='e.g onions'
+         name="ingridients" />
         <button>Add ingridient</button>
         </form> 
 
